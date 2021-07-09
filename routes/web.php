2b1793621cc3c008883f477eb9920edaf5a88378
MarcoBagnasco <bagna.hbk@gmail.com>
2021-07-09 15:18:45 +0200

    add scaffolding

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+// ROUTE ADMIN
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->name('admin.')
+    ->group(function() {
+
+        // ROUTE HOME ADMIN
+        Route::get('/', 'HomeController@index')->name('home');
+
+        // ROUTE RESOURCE POSTS
+        // Route::resource('/posts', 'PostController');
+    });
+
+//FRONT OFFICE
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where("any", ".*");
